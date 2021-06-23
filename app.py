e665bddc6b42e5aab3c4d9967d395efeda080c65
TomTurbo2016 <gemology@live.at>
2021-06-23 16:02:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,21 @@ def Index():
 
 @app.route('/200', methods=['POST'])
 def Index2():
-    body = request.get_data()
-    user = 'christian.hahna@post.at'
-    password = 'AIIn ZFXk xh8V r8vx'
-    wsdl_url = 'https://at.cloud.fabasoft.com/folio/fscdav/wsdl?WEBSVC=POSTMSPORTAL_111_100_CustomerPortalWebService'
-    headers={'content-type': 'text/xml'}
-    response = requests.post(url=wsdl_url,data=body,headers=headers,auth=(user,password),verify=False,timeout=(5.0, 5.0))
-    return (str(response.content.decode("utf-8")))
+    returnMessage = 'SUCCESS'
+    try:
+        body = request.get_data()
+        user = 'christian.hahna@post.at'
+        password = 'AIIn ZFXk xh8V r8vx'
+        wsdl_url = 'https://at.cloud.fabasoft.com/folio/fscdav/wsdl?WEBSVC=POSTMSPORTAL_111_100_CustomerPortalWebService'
+        headers={'content-type': 'text/xml'}
+        response = requests.post(url=wsdl_url,data=body,headers=headers,auth=(user,password),verify=False,timeout=(15.0, 15.0))
+        fullstring = response.content.decode("utf-8")
+        substring = '<docid>'
+        fullstring.index(substring)
+    except:
+        returnMessage 'ERROR'
+    return returnMessage
+     
 
  
 
