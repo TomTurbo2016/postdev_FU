dbbd178845bac9ff2df55b634edf1be4ef0086b8
TomTurbo2016 <gemology@live.at>
2021-06-23 15:04:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ app.secret_key = os.urandom(13)
 def Index():
     return ("<h1>Hello, World!</h1>")
 
-@app.route('/200', methods=['POST'])
-def getXml2():
-	if request.method == 'POST':
-		xmlBody = request.get_data()
-		returnMessage = 'SUCCESS'
-		try:
-			returnMessage = 'test123456'
-		return returnMessage
  
 
 if __name__ == '__main__':
